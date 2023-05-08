d0cdbcc61d3e0de2d965192b202c190d431ce9b2
Arsalaann <arsalankhanxk@gmail.com>
2023-05-09 00:00:37 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace AddressBook{
+    class AddressBookMain{
+        public static void Main(string [] args){
+            Console.WriteLine("Welcome to Address Book Program");
+        }
+    }
+}
